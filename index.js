e78e1269066cd68ba74d1b5a927ea0f91a2a0f73
Giang 112000 <giangnt.b18dcpt065.ptit@gmail.com>
2022-04-22 13:16:52 +0700

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const cors = require("cors");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const dbnoti = require("./firebase");
@@ -12,6 +13,11 @@ const {
     remove,
     update,
 } = require("firebase/database");
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 const io = new Server(server, {
     cors: {
         origin: "*",
