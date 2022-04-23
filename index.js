6f341756f69512b447a8a8ecefba386fc427008a
Giang 112000 <giangnt.b18dcpt065.ptit@gmail.com>
2022-04-23 22:02:02 +0700

    make it better

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const {
     remove,
     update,
 } = require("firebase/database");
-app.use(
-    cors({
-        origin: "*",
-    })
-);
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+app.use(express.json());
 const io = new Server(server, {
     cors: {
         origin: "*",
